10278b6c4be2ebc1dd3104ad00fbd61ce568fefc
Vitor Marinho Faria <vitorfariam97@gmail.com>
2022-05-19 23:35:27 -0300

    separate implementation in it's own file

--- main.cpp
+++ main.cpp
@@ -8,76 +8,33 @@
 
 #include <entt/entt.hpp>
 
-#define REGCLASS(type_name)                                            \
+#include "NoVirtual.hpp"
+#include "WithVirtual.hpp"
+
+#define REGCLASS_VIRT(type_name)                                                   \
     RegisteredClasses[#type_name] = [](entt::registry &reg, entt::entity entity) { \
-        NativeScriptComponent component;                               \
-        component.Bind<type_name>();                                   \
-        reg.emplace<NativeScriptComponent>(entity, component);         \
+        wivirt::NativeScriptComponent component;                                   \
+        component.Bind<type_name>();                                               \
+        reg.emplace<wivirt::NativeScriptComponent>(entity, component);             \
+    };
+#define REGCLASS_NOVIRT(type_name)                                                 \
+    RegisteredClasses[#type_name] = [](entt::registry &reg, entt::entity entity) { \
+        novirt::NativeScriptComponent component;                                   \
+        component.Bind<type_name>();                                               \
+        reg.emplace<novirt::NativeScriptComponent>(entity, component);             \
     };
 
-struct Behavior
-{
-    entt::entity m_Entity;
-    virtual void OnTick(float){};
-    virtual void OnCreate(){};
-    virtual void OnDestroy(){};
-};
-
-struct NativeScriptComponent
-{
-    Behavior *m_Instance = nullptr;
-
-    Behavior *(*InstantiateScript)();
-    void (*DestroyScript)(NativeScriptComponent *);
-
-    template <typename T>
-    void Bind()
-    {
-        InstantiateScript = []()
-        { return static_cast<Behavior *>(new T()); };
-        DestroyScript = [](NativeScriptComponent *nsc)
-        { delete nsc->m_Instance; nsc->m_Instance = nullptr; };
-    }
-};
-
-struct Camera : public Behavior
-{
-    void OnTick(float t)
-    {
-        std::cout << "Camera::OnTick Delta: " << std::fixed << std::setprecision(2) << t << "ms\n";
-    }
-    void OnCreate()
-    {
-        std::cout << "Camera::OnCreate\n";
-    }
-    void OnDestroy()
-    {
-        std::cout << "Camera::OnDestroy\n";
-    }
-};
-
-struct Entity : public Behavior
-{
-    void OnTick(float t)
-    {
-        std::cout << "Entity::OnTick Delta: " << std::fixed << std::setprecision(2) << t << "ms\n";
-    }
-    void OnCreate()
-    {
-        std::cout << "Entity::OnCreate\n";
-    }
-    void OnDestroy()
-    {
-        std::cout << "Entity::OnDestroy\n";
-    }
-};
 typedef std::function<void(entt::registry &, entt::entity)> AddClassComponentFunction;
 std::map<std::string, AddClassComponentFunction> RegisteredClasses;
 
 int main(int argc, char **argv)
 {
-    REGCLASS(Camera);
-    REGCLASS(Entity);
+    std::cout << "Press Enter to begin\n";
+    std::cin.get();
+
+    REGCLASS_VIRT(wivirt::Camera);
+    REGCLASS_VIRT(wivirt::Entity);
+    REGCLASS_NOVIRT(novirt::Camera);
 
     entt::registry registry;
 
@@ -85,27 +42,48 @@ int main(int argc, char **argv)
     AddClassComponentFunction addComponentToEntity = RegisteredClasses[argv[1]];
     addComponentToEntity(registry, createdEntity);
 
-    auto view = registry.view<NativeScriptComponent>();
+    auto view = registry.view<wivirt::NativeScriptComponent>();
     for (auto [entity, scriptComponent] : view.each())
     {
         scriptComponent.m_Instance = scriptComponent.InstantiateScript();
         scriptComponent.m_Instance->m_Entity = entity;
         scriptComponent.m_Instance->OnCreate();
     }
+    auto viewNoVirt = registry.view<novirt::NativeScriptComponent>();
+    for (auto [entity, scriptComponent] : viewNoVirt.each())
+    {
+        scriptComponent.m_Instance = scriptComponent.CreateInstance();
+        scriptComponent.m_Instance->m_Entity = entity;
+        scriptComponent.m_Instance->OnCreate(scriptComponent.m_Instance);
+    }
 
     auto begin = std::chrono::steady_clock::now();
+    // OPTICK_START_CAPTURE();
     while (true)
     {
         auto currentTime = std::chrono::steady_clock::now();
         auto deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - begin);
         begin = currentTime;
 
-        auto view = registry.view<NativeScriptComponent>();
+        auto view = registry.view<wivirt::NativeScriptComponent>();
         for (auto [entity, scriptComponent] : view.each())
         {
+            auto preTick = std::chrono::steady_clock::now();
             scriptComponent.m_Instance->OnTick(deltaTime.count());
+            auto postTick = std::chrono::steady_clock::now();
+            auto delta = std::chrono::duration_cast<std::chrono::microseconds>(postTick - preTick);
+            std::cout << delta.count() << '\n';
+        }
+        auto viewNoVirt = registry.view<novirt::NativeScriptComponent>();
+        for (auto [entity, scriptComponent] : viewNoVirt.each())
+        {
+            auto preTick = std::chrono::steady_clock::now();
+            scriptComponent.m_Instance->OnTick(scriptComponent.m_Instance, deltaTime.count());
+            auto postTick = std::chrono::steady_clock::now();
+            auto delta = std::chrono::duration_cast<std::chrono::microseconds>(postTick - preTick);
+            std::cout << delta.count() << '\n';
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(16));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(16));
     }
 }
